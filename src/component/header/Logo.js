3f809ece9191eb885bcbfad61bd7d903e99f46ff
Tselmen Azjargal <110718202+skrTR@users.noreply.github.com>
2023-02-26 20:13:50 +0800

    s

--- src/component/header/Logo.js
+++ src/component/header/Logo.js
@@ -6,7 +6,7 @@ const Logo = ({ imageUrl, logoClass }) => {
   return (
     <div className={clsx(logoClass)}>
       <Link to={process.env.PUBLIC_URL + "/"}>
-        <img alt="" src={process.env.PUBLIC_URL + imageUrl} style={{width:"40px", height: "40px"}} />
+        <img alt="" src={process.env.PUBLIC_URL + imageUrl}  />
       </Link>
     </div>
   );
